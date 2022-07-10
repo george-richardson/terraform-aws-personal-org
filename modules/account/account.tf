584d4f3ec60e675766aaed30a5aed8a35c3fd133
George Richardson <george@gjhr.me>
2022-07-10 16:02:49 +0100

    Fix account iam_user_access_to_billing imports

--- modules/account/account.tf
+++ modules/account/account.tf
@@ -2,6 +2,6 @@ resource "aws_organizations_account" "account" {
   name                       = var.name
   email                      = var.email
   parent_id                  = var.parent_id
-  iam_user_access_to_billing = "ALLOW"
+  iam_user_access_to_billing = var.iam_user_access_to_billing
   close_on_deletion          = var.close_on_deletion
 }
